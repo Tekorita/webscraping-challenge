764a5ce3e9bdac0f4e2297890808472a185dcadc
Tekorita <davidpalmalugo@gmail.com>
2019-07-16 12:24:18 -0400

    start branch with first changes Autor:Tekorita(David)

--- webscraping.py
+++ webscraping.py
@@ -1,101 +1,79 @@
-from bs4 import BeautifulSoup
 import requests
-import re
-
-
-main_url = 'http://books.toscrape.com/index.html'
-source = requests.get(main_url).text
-soup = BeautifulSoup(source, 'lxml')
-
-def getURL(url):
-    resultado = requests.get(url)
-    soup = BeautifulSoup(resultado.text, 'html.parser')
-    return(soup)
-
-#metodo para obtener las url de cada libro
-def get_urls_libros(url):
-
-    soup = getURL(url)
-
-    return(['/'.join(url.split('/')[:-1]) + '/' + x.div.a.get('href') for x in soup.find_all('article', class_='product_pod')])
-
-
-#lista para almacenar las url de cada pagina
-urls_paginas = []
-
-paginas = [str(i) for i in range (1, 51)]
-
-print("Recolectando urls de paginas...")
-for pagina in paginas:
-
-    pagina_sgte = 'http://books.toscrape.com/catalogue/page-' + pagina + '.html'
-    if requests.get(pagina_sgte).status_code == 200:
-        urls_paginas.append(pagina_sgte)
-
-print(str(len(urls_paginas)) + ' paginas recolectadas')
-print("5 primeras url: ")
-print(urls_paginas[:5])
-
-urls_libros = []
-
-for pagina in urls_paginas:
-    urls_libros.extend(get_urls_libros(pagina))
-
-print(str(len(urls_libros)) + ' links de libros recolectados')
-print("5 primeras url: ")
-print(urls_libros[:5])
-
-#listas para guardar los datos:
-
-titulos = []
-precios = []
-stock = []
-categorias = []
-urls_covers = []
-reviews = []
-upcs = []
-tipo_producto = []
-precios_sin_tax = []
-precios_con_tax = []
-tax = []
-disponibilidades = []
-num_reviews = []
-
-print("Recabando datos...")
-#se itera por cada url de cada libro y se recaban los datos
-for url in urls_libros:
-
-    soup = getURL(url)
-    titulos.append(soup.find('div', class_= re.compile('product_main')).h1.text)
-    precios.append(soup.find('p', class_='price_color').text[2:])
-    stock.append(re.sub("[^0-9]", "", soup.find('p', class_='instock availability').text))
-    categorias.append(soup.find('a', href = re.compile('../category/books/')).get('href').split('/')[3])
-    urls_covers.append(url.replace('index.html', '') + soup.find('img').get('src'))
-    reviews.append(soup.find('p', class_= re.compile('star-rating')).get('class')[1])
-
-    table = soup.find('table', class_='table table-striped')
-    datos_tabla = []
-    
-    for tr in table.find_all('tr'):
-        for td in tr.find_all('td'):
-            datos_tabla.append(td.text)
-
-    upcs.append(datos_tabla[0])
-    tipo_producto.append(datos_tabla[1])
-    precios_sin_tax.append(datos_tabla[2][2:])
-    precios_con_tax.append(datos_tabla[3][2:])
-    tax.append(datos_tabla[4][2:])
-    disponibilidades.append(datos_tabla[5])
-    num_reviews.append(datos_tabla[6])
-
 
 import pandas as pd
 
+from bs4 import BeautifulSoup
+ 
+url_base = 'http://books.toscrape.com/'
+url_archivos = url_base + '/archivos' 
+page = requests.get(url_base)
+soup = BeautifulSoup(page.text, 'html.parser')
 
-datos = pd.DataFrame({'TITLE': titulos, 'PRICE': precios, 'STOCK': stock, 'CATEGORY': categorias, 'COVER': urls_covers, 'UPC': upcs, 'PRODUCT_TYPE': tipo_producto, 'PRICE (excl tax)': precios_sin_tax, 'PRICE (incl tax)': precios_con_tax, 'TAX': tax, 'AVAILABILITY': disponibilidades, 'N° OF REVIEWS': num_reviews, 'reviews': reviews})
-datos.head()
+def export_csv(scrap):
+    """Exportamos el json scraper en formato csv"""
+    df = pd.DataFrame(scrap)
+    df.to_csv('scraperBooks.csv', sep='\t', encoding='utf-8')
+    print("scraper exportado a formato csv con exito   ")
 
-print("Generando CSV...")
-datos.to_csv('datos_recolectados.csv', encoding='utf-8')
+def get_links():
+    """Recorremos el link de cada libro"""
+    href_links = []
+    for href in soup.find_all(class_="image_container"):
+        image = href.find('a', href=True)
+        href_links.append(image['href'])
+    
+    return href_links
+
+def get_content(links):
+    """Extraemos el contenido de los items del libro para crear el scrapper"""
+    product_infor = []
+    libro = {}
+    product_description = {}
+    scrap_web = []
+
+    for link in links:
+        page = requests.get(url_base + link)
+        soup = BeautifulSoup(page.text, 'html.parser')
+        title = soup.find(class_='col-sm-6 product_main').find("h1").text
+        price = soup.find(class_='col-sm-6 product_main').find(class_="price_color").text
+        stock = soup.find(class_='col-sm-6 product_main').find(class_="instock availability").text
+        category = soup.find(class_='breadcrumb').find_all("a")[2].text
+        cover = soup.find("img")
+
+        libro['Title'] = title
+        libro['Price'] = price
+        libro['Stock'] = stock.strip()
+        libro['Category'] = category
+        libro['Cover'] = cover['src']
+
+        for items in soup.find(class_='table table-striped').find_all("td"):
+            """Iteramos la tabla de la descripcion del productos para traer los items"""
+            product_infor.append(items.getText())
+
+            if len(product_infor) == 7:           
+                product_description['UPC'] = product_infor[0]
+                product_description['Product Type'] = product_infor[1]
+                product_description['Price (excl. tax)'] = product_infor[2]
+                product_description['Price (incl. tax)'] = product_infor[3]
+                product_description['Tax'] = product_infor[4]
+                product_description['Availability'] = product_infor[5]
+                product_description['Number of reviews'] = product_infor[6]
+                libro['Product Description'] = product_description          
+                product_infor = []
+                
+        print(libro)  
+        scrap_web.append(libro)
+        print("se guardo todo el contenido de un libro en el json")
+        print("---------------------------------------------------------------------------------------")
+    #print(scrap_web)
+    """Exportamos el scrap en formato csv"""
+    export_csv(scrap_web)
+
+urls_books = get_links()
+
+get_content(urls_books)
+
+
+
 
 
